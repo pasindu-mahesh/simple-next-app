a996dd2a6d1018b5ce2ad45086ed786b8c3a0c6a
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-03-13 21:08:32 +0530

    newspecs

--- new_app/src/app/prediction/[name]/page.tsx
+++ new_app/src/app/prediction/[name]/page.tsx
@@ -1,12 +1,43 @@
+const getPredictedAge = async (name: string) => {
+    const res = await fetch(`https://api.agify.oi/?name=${name}`);
+    return res.json();
+};
+
+const getPredictedGender = async (name: string) => {
+    const res = await fetch(`https://api.agify.oi/?name=${name}`);
+    return res.json();
+};
+
+const getPredictedCountry = async (name: string) => {
+    const res = await fetch(`https://api.agify.oi/?name=${name}`);
+    return res.json();
+};
+
+
+import { Params } from "next/dist/server/request/params";
+
 interface params{
     params: {name: string};
 }
 
-export default function page({params}: any) {
+export default async function page({params}: params) {
+    const ageData = getPredictedAge(params.name);
+    const genderData = getPredictedGender(params.name);
+    const countryData = getPredictedCountry(params.name);
+
+    const [age, gender, country] = await Promise.all([
+        ageData, 
+        genderData, 
+        countryData
+    ]);
     return(
-        <main>
-            {" "}
-            {params.name}
-        </main>
+        <div>
+            <div>
+                <div> Personal Info </div>
+                <div> Age: {age?.age}</div>
+                <div> Gender: {gender?.gender}</div>
+                <div> Country: {country?.country[0]?.country_id}</div>
+            </div>
+        </div>
     );
 }
